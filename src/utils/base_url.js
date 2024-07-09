25c0faa3e71e50947818108d5cec3a9531005fa1
Abhay Pratap Yadav <128727269+abhi-412@users.noreply.github.com>
2024-07-09 17:19:31 +0530

    Update base_url.js

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-// export const base_url = "https://shopme-server-eight.vercel.app/api/";
-export const base_url = "http://localhost:3000/api/";
+export const base_url = "https://shopme-server-eight.vercel.app/api/";
+// export const base_url = "http://localhost:3000/api/";
